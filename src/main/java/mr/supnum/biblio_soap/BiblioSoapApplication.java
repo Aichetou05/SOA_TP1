a06387ed59fbf2cd1593670e86ece892ee070a44
Aichetou05 <23021@supnum.mr>
2025-11-21 23:46:34 +0100

    initial commit

---
+++ src/main/java/mr/supnum/biblio_soap/BiblioSoapApplication.java
@@ -0,0 +1,13 @@
+package mr.supnum.biblio_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mr/supnum/biblio_soap/BiblioSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package mr.supnum.biblio_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
